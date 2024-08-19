26f8e90e2e18740591971ddab5c661dfe6dc4244
Teczohaib <malikwasi77778686@gmail.com>
2024-08-19 20:26:37 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+Class Main{
+    public static void main (String[] args)
+{
+    boolean sunny=true;
+    boolean hot=false;
+    if(sunny){
+        if(hot){
+            System.out.println("wear cap and half sleevs t shirt");
+        }
+            else{
+                System.out.println("wear half sleevs t shirt");
+            }
+                else{
+                    System.out.println("wear a raincoat");
+                }
+            }
+
+        }
+    }
+    
+    
+
+
+
