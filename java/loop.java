6a6db6474a30fba5a36ace9ae83833803cc024a5
Teczohaib <malikwasi77778686@gmail.com>
2024-08-20 19:01:42 +0500

    Add files via upload

---
+++ java/loop.java
@@ -0,0 +1,18 @@
+
+
+class loop{
+    public static void main(String[] args) {
+        int i=10;
+        System.out.println("desanding order");
+        for(i=10;i>=1;i--){
+            System.out.println(i);
+        }
+        System.out.println("Asceanding order");
+        int c=1;
+        while(c<10){
+            System.out.println(c);
+            c=c+1;
+        }
+
+    }
+}
